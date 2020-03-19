368844aa5b4aa366a60e56531d937c3cf32fd2b5
kaye-h <541331088@qq.com>
2020-03-19 13:24:49 +0800

    extract github config from code to propreties

--- src/main/java/com/liangzai/community/coinfig/WebConfig.java
+++ src/main/java/com/liangzai/community/coinfig/WebConfig.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //将所有/static/** 访问都映射到classpath:/static/ 目录下
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
