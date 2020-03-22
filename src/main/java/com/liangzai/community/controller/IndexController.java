c92f0c6364f24c8d7e675ec6691745af9c0a47b4
kaye-h <541331088@qq.com>
2020-03-22 21:16:53 +0800

    add question

--- src/main/java/com/liangzai/community/controller/IndexController.java
+++ src/main/java/com/liangzai/community/controller/IndexController.java
@@ -19,6 +19,7 @@ public class IndexController {
     public String index(HttpServletRequest request){
 
         Cookie[] cookies = request.getCookies();
+        if (cookies !=null && cookies.length !=0){
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals("token")){
                 String token = cookie.getValue();
@@ -29,7 +30,7 @@ public class IndexController {
                 break;
             }
         }
-
+        }
         return "index";
     }
 }
