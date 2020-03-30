07bfa90e8d88bda130d5f9161cad1a8d5d4bfddd
kaye-h <541331088@qq.com>
2020-03-30 14:33:21 +0800

    æ·使用逆向工程重ææ构é

--- src/main/java/com/liangzai/community/CommunityApplication.java
+++ src/main/java/com/liangzai/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package com.liangzai.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.liangzai.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
